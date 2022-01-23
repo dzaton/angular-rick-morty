6d030d05cdb9ea934eb1695682c7ef0365c0ab55
dzaton <dzatonsaiz@gmail.com>
2022-01-23 01:23:13 +0100

    busqueda de personajes

--- src/app/components/pages/characters/character-list/character-list.component.ts
+++ src/app/components/pages/characters/character-list/character-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Params } from '@angular/router';
 import { Character } from '@app/shared/interfaces/character.interface';
 import { CharacterService } from '@app/shared/services/character.service';
 import {take} from 'rxjs/operators'
@@ -19,26 +20,40 @@ export class CharacterListComponent implements OnInit {
 
 
   private pageNum=1;
-  private query: string ='';
+  private query: string='';
   private hodeScrollHeight =200;
   private showScrollHeight = 500;
 
-  constructor(private characterSvc: CharacterService) { }
+  constructor(private characterSvc: CharacterService, 
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getDataFromService();
+    this.getCharactersByQuery()
+    //this.getDataFromService();
   }
 
+  private getCharactersByQuery(): void {
+    this.route.queryParams.pipe(take(1)).subscribe((params: Params) => {
+      this.query = params['q'];
+      this.getDataFromService();
+    });
+  }
 
 private getDataFromService (): void{
   this.characterSvc
     .searchCharacters(this.query, this.pageNum)
     .pipe(take(1))
         .subscribe((res:any)=>{
-          console.log('Response ->',res)
-          const{info, results}= res;
-          this.characters=[...this.characters, ...results]
-          this.info=info;
+          // ? optional chainning para comprobar que las propiedades existan
+          if(res?.results?.length){
+            //console.log('Response ->',res)
+            const{info, results}= res;
+            this.characters=[...this.characters, ...results]
+            this.info=info;
+          }else{
+            this.characters=[]
+          }
+          
         });
 
 }
